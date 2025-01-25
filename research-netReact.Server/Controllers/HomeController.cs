ef3943d2070018c1d2f8d07377d1dd8f7f44be0f
Bruno Koppel <brunokoppel.dev@gmail.com>
2025-01-24 23:43:02 -0500

    fix

---
+++ research-netReact.Server/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace research_netReact.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Here"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetHome")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = "None",
+                TemperatureC = 1,
+                Summary = "Test"
+            })
+            .ToArray();
+        }
+    }
+}
