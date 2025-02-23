2f68da253a822a6739c62225d31fa31ba3deb685
Bruno Koppel <brunokoppel.dev@gmail.com>
2025-02-23 00:00:33 -0500

    latest work

--- research-netReact.Server/Models/TodoItem.cs
+++ research-netReact.Server/Models/TodoItem.cs
@@ -3,7 +3,13 @@
     public class TodoItem
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public bool IsCompleted { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; } = false;
+
+        public TodoItem(string title, bool isCompleted)
+        {
+            IsCompleted = isCompleted;
+            Title = title;
+        }
     }
 }
